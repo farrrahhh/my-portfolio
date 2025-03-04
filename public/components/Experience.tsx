b8834cc6502592f3ab6bbf3752dc76097f532d0e
farrrahhh <18222096@std.stei.itb.ac.id>
2025-03-04 08:28:07 +0700

    Add import statement for VerticalTimeline and its styles in Experience component

--- public/components/Experience.tsx
+++ public/components/Experience.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
